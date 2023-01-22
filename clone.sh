c2a39fa0d805f5b65a33816fc194979ce0a2aa9d
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-23 03:41:47 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -12,4 +12,4 @@ repo sync -j$(nproc) --force-sync --no-clone-bundle --no-tags
 repo sync --force-sync
 
 # Clone Devive Tree here
-git clone ${DT} -b pbrp device/${OEM}/${DEVICE}
+git clone ${DT} -b android-13.0 device/${OEM}/${DEVICE}
