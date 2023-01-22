97ebc9b05351d10c14918ce244d2ade8325a3bc2
Nico1704 <119746762+Nico170420@users.noreply.github.com>
2023-01-22 22:05:03 +0100

    cleanup unneeded vars

--- clone.sh
+++ clone.sh
@@ -5,13 +5,11 @@ DEVICE="z3s"
 DT="https://github.com/Nico170420/android_device_samsung_z3s.git"
 OEM="samsung"
 PBRP_BRANCH="12.1"
-TARGET=(
-	pbrp
-)
 
-
+# Clone and sync Recovery Source
 repo init --depth=1 -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-${PBRP_BRANCH}
 repo sync -j$(nproc) --force-sync --no-clone-bundle --no-tags
 repo sync --force-sync
 
+# Clone Devive Tree here
 git clone ${DT} -b pbrp device/${OEM}/${DEVICE}
