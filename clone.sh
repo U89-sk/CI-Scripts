a6e0336a50c048d1fe6d037813088e772a7b5628
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-23 20:18:49 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,7 +4,7 @@
 DEVICE="RMX1851"
 DT="https://github.com/U89-sk/android_device_realme_RMX1851-pbrp.git"
 OEM="realme"
-PBRP_BRANCH="11.0"
+PBRP_BRANCH="12.1"
 
 # Clone and sync Recovery Source
 repo init --depth=1 -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-${PBRP_BRANCH}
