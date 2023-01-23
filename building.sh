6c9dc2a12c7106937b7040cafdfce2a64d1ed744
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-23 13:36:54 +0530

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Define Variables
-DEVICE="z3s"
-DT="https://github.com/Nico170420/android_device_samsung_z3s.git"
-OEM="samsung"
-TW_BRANCH="12.1"
+DEVICE="RMX1851"
+DT="https://github.com/U89-sk/android_device_realme_RMX1851-twrp.git"
+OEM="realme"
+TW_BRANCH="11"
 TARGET=(
 	recoveryimage
 )
