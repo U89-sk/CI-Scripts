665adbc701e0c7c08d5c275ed1a5d31337746700
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-23 13:41:23 +0530

    Update upload2.sh

--- upload2.sh
+++ upload2.sh
@@ -12,7 +12,7 @@ fi
 
 file_content_type="application/octet-stream"
 files_to_upload=(
-/tmp/cirrus-ci-build/out/target/product/z3s/SHRP*.zip
+/tmp/cirrus-ci-build/out/target/product/RMX1851/recovery.img
 )
 
 for fpath in $files_to_upload
