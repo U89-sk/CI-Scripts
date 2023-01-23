cdf73682c9e3a64cef801c7d8943ac602b7c4237
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-23 13:40:37 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Define Variables
-DEVICE="z3s"
-DT="https://github.com/Nico170420/android_device_samsung_z3s.git"
-OEM="samsung"
-SHRP_BRANCH="12.1"
+DEVICE="RMX1851"
+DT="https://github.com/U89-sk/android_device_realme_RMX1851-twrp.git"
+OEM="realme"
+SHRP_BRANCH="11"
 TARGET=(
 	recoveryimage
 )
 
-cd /tmp/cirrus-ci-build/out/target/product/z3s/
+cd /tmp/cirrus-ci-build/out/target/product/RMX1851/
 
 curl -sL https://git.io/file-transfer | sh
 ./transfer wet recovery.img
