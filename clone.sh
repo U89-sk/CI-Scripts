e138edba4d0197ad31f9c7624aad8f11ed528c92
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-23 12:04:04 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Define Variables
-DEVICE="z3s"
-DT="https://github.com/Nico170420/android_device_samsung_z3s.git"
-OEM="samsung"
+DEVICE="RMX1851"
+DT="https://github.com/U89-sk/android_device_realme_RMX1851-shrp.git"
+OEM="realme"
 SHRP_BRANCH="12.1"
 TARGET=(
 	recoveryimage
@@ -14,4 +14,4 @@ repo init --depth=1 -u https://github.com/SHRP/manifest.git  -b shrp-${SHRP_BRAN
 repo sync -j$(nproc) --force-sync --no-clone-bundle --no-tags
 repo sync --force-sync
 
-git clone ${DT} -b shrp device/${OEM}/${DEVICE}
+git clone ${DT} -b android-13.0 device/${OEM}/${DEVICE}
