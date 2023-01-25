2aefb1517778f551582e2fc7e43861bfe169303f
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-25 21:15:36 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,15 +2,26 @@
 
 # Define Variables
 DEVICE="RMX1851"
-DT="https://github.com/U89-sk/android_device_realme_RMX1851-twrp.git"
-OEM="realme"
-SHRP_BRANCH="11"
-TARGET=(
-	recoveryimage
-)
 
-cd /tmp/cirrus-ci-build/out/target/product/RMX1851/
+#cd /tmp/cirrus-ci-build/out/target/product/RMX1851/
 
+#curl -sL https://git.io/file-transfer | sh
+#./transfer wet recovery.img
+
+# Outputfolder
+Output="out/target/product/${Device}"
+
+# Code
+if [ -a $Output/SHRP*.zip ]; then
+    Files="SHRP*.zip"
+elif [ -a $Output/PBRP*.zip ]; then
+    Files="PBRP*.zip"
+else
+    Files="recovery.img"
+fi
+
+# Download Upload Script
 curl -sL https://git.io/file-transfer | sh
-./transfer wet recovery.img
 
+# Upload
+./transfer wet ${Output}/${Files}
