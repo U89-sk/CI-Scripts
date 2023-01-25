c76bcd5b00bbc31ad9b51ccc44c77a8d135f696a
U89-sk <107832346+U89-sk@users.noreply.github.com>
2023-01-25 21:11:43 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,14 +4,14 @@
 DEVICE="RMX1851"
 DT="https://github.com/U89-sk/android_device_realme_RMX1851-twrp.git"
 OEM="realme"
-TW_BRANCH="11"
+TW_BRANCH="9.0"
 TARGET=(
 	recoveryimage
 )
 
 
-repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-${TW_BRANCH}
+repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni -b twrp-${TW_BRANCH}
 repo sync -j$(nproc) --force-sync --no-clone-bundle --no-tags
 repo sync --force-sync
 
-git clone ${DT} -b a11-RUI2 device/${OEM}/${DEVICE}
+git clone ${DT} -b android-9.0 device/${OEM}/${DEVICE}
